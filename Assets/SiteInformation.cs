db25950f687e0d07db0a43b920e16dc8496ce611
Simond Lee <simondlee@gmail.com>
2018-08-16 16:32:26 +1000

    Added information text for sites

---
+++ Assets/SiteInformation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Models.SteelConnect;
+public class SiteInformation : MonoBehaviour {
+    private GameObject siteObject;
+
+	// Use this for initialization
+	void Start () {
+        siteObject = transform.parent.gameObject;
+        SiteMarker siteMarker = siteObject.GetComponent<SiteMarker>();
+        Site site = siteMarker.Site;
+        string text = $"Id: {site.id}\n" +
+                      $"Name: {site.name}\n" +
+                      $"Longname: {site.longname}\n" +
+                      $"Org: {site.org}\n" +
+                      $"Country: {site.country}\n" +
+                      $"City: {site.city}\n" +
+                      $"Street Address: {site.street_address}";
+        TextMesh textMesh = GetComponent<TextMesh>();
+        textMesh.text = text;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
