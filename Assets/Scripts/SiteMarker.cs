74f6d713a88662b5f3c9f3b03348747bb87e1c76
Simond Lee <simondlee@gmail.com>
2018-08-16 15:54:08 +1000

    Added site size increase with pointer hover

--- Assets/Scripts/SiteMarker.cs
+++ Assets/Scripts/SiteMarker.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 using Proyecto26;
 using Models.SteelConnect;
 
-public class SiteMarker : MonoBehaviour {
+public class SiteMarker : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerClickHandler{
     public Site Site;
+    public float hoverIncreaseScale = 0.02f;
 
     // Use this for initialization
     void Start() {
@@ -16,5 +18,23 @@ public class SiteMarker : MonoBehaviour {
     // Update is called once per frame
     void Update() {
 
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        // Make size larger
+        transform.localScale += new Vector3(hoverIncreaseScale, hoverIncreaseScale, hoverIncreaseScale);
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        // Revert to original size
+        transform.localScale -= new Vector3(hoverIncreaseScale, hoverIncreaseScale, hoverIncreaseScale);
+    }
+
+    // Need this event for PointerClick
+    public void OnPointerDown(PointerEventData eventData) {
+    }
+
+    public void OnPointerClick(PointerEventData eventData) {
+        Debug.Log("Click " + Site.id);
     }
 }
