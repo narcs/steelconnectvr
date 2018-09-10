eedfe8798e3d4f64867720802178a9740b32a8b2
Simond Lee <simondlee@gmail.com>
2018-09-10 20:04:04 +1000

    Added part creation of uplink game objects

--- Assets/WanMarker.cs
+++ Assets/WanMarker.cs
@@ -5,14 +5,18 @@ using UnityEngine.EventSystems;
 
 using Models.SteelConnect;
 
-public class WanMarker : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler {
+public class WanMarker : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler {
 
     public Wan wan;
     public GameObject cloud;
     public GameObject information;
+    public GameObject uplinkPrefab;
+    public GameObject reticle;
 
     private Behaviour _halo;
     private MeshRenderer _informationMeshRenderer;
+    private GameObject _currentUplinkCreation = null;
+    private List<GameObject> _uplinks = new List<GameObject>();
 
 	void Start () {
         _halo = (Behaviour)cloud.GetComponent("Halo");
@@ -22,6 +26,22 @@ public class WanMarker : MonoBehaviour, IPointerEnterHandler, IPointerExitHandle
 	}
 	
 	void Update () {
+        // Position uplink line from WAN to pointer
+        if (_currentUplinkCreation) {
+            LineRenderer lineRenderer = _currentUplinkCreation.GetComponent<LineRenderer>();
+            lineRenderer.positionCount = 2;
+            Vector3[] points = new Vector3[2] {
+                transform.position,
+                reticle.transform.position
+            };
+            lineRenderer.SetPositions(points);
+        }
+        // Position uplink line to follow WANs
+        // TODO: Optimise
+        foreach (GameObject uplink in _uplinks) {
+            LineRenderer lineRenderer = uplink.GetComponent<LineRenderer>();
+            lineRenderer.SetPosition(0, transform.position);
+        }
 		
 	}
 
@@ -36,14 +56,14 @@ public class WanMarker : MonoBehaviour, IPointerEnterHandler, IPointerExitHandle
     }
 
     public void OnPointerDown(PointerEventData eventData) {
-        // Render uplink line from WAN to pointer
+        // Create uplink GameObject
+        _currentUplinkCreation = Instantiate(uplinkPrefab, transform);
     }
 
     public void OnPointerUp(PointerEventData eventData) {
-        // Create uplink if finish on site
-    }
-
-    public void OnPointerClick(PointerEventData eventData) {
-        // Create uplink
+        // Create uplink if selecting site
+        _uplinks.Add(_currentUplinkCreation);
+        _currentUplinkCreation = null;
+        // Create uplink API call
     }
 }
