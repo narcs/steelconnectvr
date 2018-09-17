61ecdd94bf7071eaab3ee53ac5e25e5178997971
Richard <rpha6@student.monash.edu>
2018-09-17 21:55:16 +1000

    Added mapbox integration stuff.

---
+++ Assets/CameraDrag.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDrag : MonoBehaviour {
+
+    //public float dragSpeed = 2;
+    public float rotateSpeed = 3.5f;
+    //private Vector3 dragOrigin;
+    private float X, Y;
+
+	
+	// Update is called once per frame
+	void Update () {
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    dragOrigin = Input.mousePosition;
+        //    return;
+        //}
+
+        //if (Input.GetMouseButton(0))
+        //{
+        //    Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+        //    Vector3 move = new Vector3(pos.x * -dragSpeed, 0, pos.y * -dragSpeed);
+
+        //    transform.Translate(move, Space.World);
+        //}
+
+        // Rotation code if mouse is dragged
+        if (Input.GetMouseButton(1))
+        {
+            transform.Rotate(new Vector3(Input.GetAxis("Mouse Y") * rotateSpeed, -Input.GetAxis("Mouse X") * rotateSpeed, 0));
+
+            X = transform.rotation.eulerAngles.x;
+            Y = transform.rotation.eulerAngles.y;
+            transform.rotation = Quaternion.Euler(X, Y, 0);
+        }
+        // Transform code if keys are pressed
+        if (Input.GetKey("w"))  // Forward
+        {
+            Vector3 translate = Camera.main.transform.forward * 1.0f;
+            transform.Translate(translate);
+        }
+        if (Input.GetKey("s")) // Back
+        {
+            Vector3 translate = Camera.main.transform.forward * -1.0f;
+            transform.Translate(translate);
+        }
+        if(Input.GetKey("q"))  // Up
+        {
+            Vector3 translate = Camera.main.transform.up * 1.0f;
+            transform.Translate(translate);
+        }
+        if (Input.GetKey("e")) // Down
+        {
+            Vector3 translate = Camera.main.transform.up * -1.0f;
+            transform.Translate(translate);
+        }
+        if (Input.GetKey("a")) // Left
+        {
+            Vector3 translate = Camera.main.transform.right * -1.0f;
+            transform.Translate(translate);
+        }
+        if (Input.GetKey("d")) // Right
+        {
+            Vector3 translate = Camera.main.transform.right * 1.0f;
+            transform.Translate(translate);
+        }
+
+    }
+}
---
+++ Assets/RayCast.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mapbox.Unity.Map;
+using Mapbox.Unity.Utilities;
+using Mapbox.Utils;
+
+public class RayCast : MonoBehaviour {
+    // Flat Map
+    [SerializeField]
+    AbstractMap _mapManager;
+    // Globe
+    [SerializeField]
+    GlobeTileProvider _globeMap;
+    
+    [SerializeField]
+    Transform _objectToRotate;
+
+    [SerializeField]
+    float _multiplier;
+
+    Vector3 _startTouchPosition;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+            int layerMask = 1 << 8;
+            layerMask = ~layerMask;
+
+            RaycastHit hit;
+            Quaternion rotation = _globeMap.transform.rotation;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+            {
+                Debug.DrawRay(ray.origin, ray.direction * 1000, Color.magenta, 200f);
+                Vector3 position = hit.point;
+                if (Input.GetMouseButtonDown(0))
+                {
+                    if (hit.collider.gameObject.layer == _globeMap.gameObject.layer)
+                    {
+                        position = Quaternion.Inverse(rotation) * position;
+
+                        Vector2d geo = Conversions.GeoFromGlobePosition(position, 100);
+                        _mapManager.UpdateMap(new Mapbox.Utils.Vector2d(geo.x, geo.y), _mapManager.Zoom);
+                        //Debug.Log(hit.point);
+                    
+                        _startTouchPosition = Input.mousePosition;
+                    }
+                }
+                if (Input.GetMouseButton(0))
+                {
+                    if (hit.collider.gameObject.layer == _globeMap.gameObject.layer)
+                    {
+                        var dragDelta = Input.mousePosition - _startTouchPosition;
+                        var axis = new Vector3(0f, -dragDelta.x * _multiplier, 0f);
+                        _objectToRotate.RotateAround(_objectToRotate.position, axis, _multiplier);
+                    }
+                }
+            }
+            else
+            {
+                //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+                //Debug.Log("Did not Hit");
+            }
+    }
+}
